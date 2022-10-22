d2f671783288959cb8fb8be3cbf8268e0f5f3d2e
roadgras <asa881977828@gmail.com>
2022-10-22 05:31:06 -0700

    Initial Commit

---
+++ capoo/capooApp.swift
@@ -0,0 +1,17 @@
+//
+//  capooApp.swift
+//  capoo
+//
+//  Created by li on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct capooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
